a5d1d11986b2c52e218e8463f17c1588dbd956e7
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-04-15 11:54:13 +0300

    #2 изменения в публикация.

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { useQueryStringValue } from "./hooks/use-query-string-value";
 export { boolMiceAdapter } from "./hooks/mice-adapter-bool";
 export { dateMiceAdapter } from "./hooks/mice-adapter-date";
 export { moneyMiceAdapter } from "./hooks/mice-adapter-money";
-export { stringToMice } from "./hooks/mice-adapter-string";
+export { stringMiceAdapter } from "./hooks/mice-adapter-string";
+export { useLocationEffect } from "./hooks/use-location-effect";
