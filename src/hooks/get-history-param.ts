fe6e9f2d1350a1949eccf664244e0096dab09b17
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-09-22 13:15:39 +0300

    resolves #29

--- src/hooks/get-history-param.ts
+++ src/hooks/get-history-param.ts
@@ -1,6 +1,15 @@
+import { urlToString } from "../utils/url-to-string";
 import { getCleanStringFromParams } from "./get-clean-string-from-params";
 import { Relaxed } from "./type-defs";
 
-export const getHistoryParam = (search: string, name: string): Relaxed<string> => {  
-  return getCleanStringFromParams(new URLSearchParams(search),name);
+export const getHistoryParam = (search: string, name: string): Relaxed<string> => {
+  return getCleanStringFromParams(new URLSearchParams(search), name);
+}
+
+export const getAllParams = (search: string): Record<string, Relaxed<string>> => {
+  const p = new URLSearchParams(search);
+  return [...p.keys()].reduce((a, b) => {
+    a[b] = urlToString(p.get(b))
+    return a;
+  }, {} as Record<string, Relaxed<string>>);
 }
